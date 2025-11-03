5f4d6c86c78448a83e289e7a947a016420919de8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-02 20:57:08 -0400

    Add files via upload

---
+++ cursor-appimage.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -eu
+
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=Cursor-"$VERSION"-anylinux-"$ARCH".AppImage
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_PIPEWIRE=1
+export OPTIMIZE_LAUNCH=1
+
+# DEPLOY ALL LIBS
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun  \
+	./AppDir/bin/*             \
+	/usr/lib/libnss*           \
+	/usr/lib/libsoftokn3.so    \
+	/usr/lib/libfreeblpriv3.so \
+	/usr/lib/pkcs11/*
+
+# Weird
+ln -s ../bin/chrome ./AppDir/shared/bin/exe
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
+mkdir -p ./dist
+mv -v ./*.AppImage*  ./dist
+mv -v ~/version      ./dist
+echo "All Done!"
